1e25ec7b01c8c6a403b87fd443eef8456afa55e9
S. SAHIL <sahilshaik2849@gmail.com>
2026-05-07 12:55:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 import express from "express";
 const app = express();
+const port = 3000;
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(port, () => {
+  console.log(`server is running on port ${port}.`);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h2>Contact us at 9505849241</h2>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<p>We are a company that provides web development services.</p>");
 });
