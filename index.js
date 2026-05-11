b04f9cd73f90dfd3b5f41caf9688769840438485
S. SAHIL <sahilshaik2849@gmail.com>
2026-05-11 12:58:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-//To see how the final website should work, run "node solution.js".
-//Make sure you have installed all the dependencies with "npm i".
-//The password is ILoveProgramming
+import express from "express";
+import ejs from "ejs";
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  let day = today.getDay();
+
+  let dayType = "weekday";
+  let advice = "week day so stay active and work hard";
+
+  if (day === 0 || day === 6) {
+    dayType = "weekend";
+    advice = "weekend so relax and enjoy your time";
+  }
+
+  res.render("index", {
+    dayType: dayType,
+    advice: advice,
+  });
+});
