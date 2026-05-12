31ba8b40acc99218ef082f3a63703c4fb1e3aff8
S. SAHIL <sahilshaik2849@gmail.com>
2026-05-12 10:56:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 import express from "express";
-import ejs from "ejs";
-
 const app = express();
-const PORT = 3000;
-
-app.set("view engine", "ejs");
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const port = 3000;
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  let day = today.getDay();
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
+});
 
-  let dayType = "weekday";
-  let advice = "week day so stay active and work hard";
-
-  if (day === 0 || day === 6) {
-    dayType = "weekend";
-    advice = "weekend so relax and enjoy your time";
-  }
-
-  res.render("index", {
-    dayType: dayType,
-    advice: advice,
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
