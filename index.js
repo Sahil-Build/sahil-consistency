7f000bc42879dd0ac8e9beb26bb2c3fe4b688aca
S. SAHIL <sahilshaik2849@gmail.com>
2026-05-12 12:05:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  const data = {
-    title: "EJS Tags",
-    seconds: new Date().getSeconds(),
-    items: ["apple", "banana", "cherry"],
-    htmlContent: "<strong>This is some strong text</strong>",
-  };
-  res.render("index.ejs", data);
+  res.render("index");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+
+  const fullName = req.body.fName + " " + req.body.lName;
+
+  const letters = fullName.replace(" ", "").length;
+
+  res.render("index", {
+    title: `Hello ${fullName}! Your name has ${letters} letters.`,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Running on http://localhost:${port}`);
 });
