208418c3aeb34511937a428e81937226cabcb99c
S. SAHIL <sahilshaik2849@gmail.com>
2026-06-06 13:02:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
-const app = express();
+const band = express();
+band.use(express.json());
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+band.listen(8000, () => {
+  console.log("server is is running on 8000");
 });
 
-app.get("/movies", (req, res) => {
-  res.send("browsing genre:" + req.query.genre);
+band.get("/bands", (req, res) => {
+  res.send("Fetching all bands");
 });
 
-app.get("/movies/:title", (req, res) => {
-  res.send("movie: " + req.params.title + " from year: " + req.query.year);
+band.get("/bands/search", (req, res) => {
+  res.send("searching bands from: " + req.query.country);
+});
+
+band.get("/bands/:name", (req, res) => {
+  res.send("Fetching Band: " + req.params.name);
+});
+
+band.delete("/bands/:name", (req, res) => {
+  res.send("Deleted band: " + req.params.name);
+});
+
+band.post("/bands", (req, res) => {
+  const name = req.body.name;
+  const genre = req.body.genre;
+  res.send("band created: " + name + " - genre: " + genre);
 });
