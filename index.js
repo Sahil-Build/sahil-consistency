57c24ed4fbf8d02a403633c59319c7e1ab4cde54
S. SAHIL <sahilshaik2849@gmail.com>
2026-06-09 12:58:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const port = 4000;
+const port = 8080;
 
-app.listen(port, () => {
-  console.log("listening to the port", port);
+app.use((req, res, next) => {
+  console.log("request method:" + req.method + ",request path:" + req.path);
+  next();
 });
 
-const checkPassword = (req, res, next) => {
-  const password = req.query.password;
-  if (password && password == "ilovecoding") {
+const checkGrade = (req, res, next) => {
+  const grade = req.body.grade;
+  if (grade && grade <= 100 && grade >= 0) {
     next();
   } else {
-    res.send("password is wrong, try again!");
+    res.send("Invalid grade!");
   }
 };
 
-app.get("/secret", checkPassword, (req, res) => {
-  res.send("welcome, here is the secret:67");
+app.listen(port, () => {
+  console.log("listening to the port:", port);
+});
+
+app.get("/students/search", (req, res) => {
+  res.send("seaching students studying:" + req.query.subject);
+});
+
+app.get("/students", (req, res) => {
+  res.send("Fetching all the students");
+});
+
+app.get("/students/:name", (req, res) => {
+  res.send("Fetching student:" + req.params.name);
+});
+
+app.post("/students", checkGrade, (req, res) => {
+  res.send("Student added: " + req.body.name + "- Grade:" + req.body.grade);
+});
+
+app.delete("/students/:name", (req, res) => {
+  res.send("removed student:" + req.params.name);
 });
