85c9550c1f48a746ca8c24cc2eb67bc6870117b0
S. SAHIL <sahilshaik2849@gmail.com>
2026-06-10 13:00:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
-const port = "8080";
+const port = 8080;
 app.set("view engine", "ejs");
 
 app.listen(port, () => {
   console.log("listening to the port:", port);
 });
 
-app.get("/", (req, res) => {
-  res.render("index", { name: "sahil" });
-});
+app.get("/report", (req, res) => {
+  const marks = [85, 90, 78, 92, 68];
+  let result = "pass";
+  for (let i = 0; i < marks.length; i++) {
+    if (marks[i] < 75) {
+      result = "fail";
+    }
+  }
 
-app.get("/instruments", (req, res) => {
   res.render("index", {
-    items: ["guitar", "drums", "piano"],
-    plain: "<h1>hello</h1>",
+    name: "sahil",
+    marks: marks,
+    result: result,
   });
 });
