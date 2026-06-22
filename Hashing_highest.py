df6979f53f486a2fafa8ac5d54d70137209ed5c6
S. SAHIL <sahilshaik2849@gmail.com>
2026-06-22 12:22:05 +0530

    Add files via upload

---
+++ Hashing_highest.py
@@ -0,0 +1,16 @@
+arr = list(map(int,input().split()))
+dic = {}
+for items in arr:
+  if items in dic:
+    dic[items] += 1
+  else:
+    dic[items] = 1
+max_key =0
+max_value =0
+for key,value in dic.items():
+  if value == max_value:
+    if key < max_key:
+      max_key = key 
+  elif value> max_value:
+    max_key,max_value = key,value
+print(max_key,"->",max_value)
