4c9fb9d934bbd233c603f9a75b4e86abf3ccd85d
S. SAHIL <sahilshaik2849@gmail.com>
2026-06-23 12:52:25 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const app = express();
+let posts = [
+  {
+    id: 1,
+    title: "my first post",
+    content: "this is the content of my first post",
+  },
+  { id: 2, title: "my second post", content: "second post content here" },
+];
+
 const port = 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-  console.log("listening to the port:", port);
+  console.log("server is running on port:", port);
 });
 
 app.get("/", (req, res) => {
-  res.render("index", { names: [] });
+  res.render("homepage", { posts });
 });
 
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-  const names = ["sahil", "sam", "alfred"];
-  res.render("index", { names: names });
+app.get("/posts/new", (req, res) => {
+  res.render("newPost");
+});
+
+app.get("/posts/:id", (req, res) => {
+  const post = posts.find(function (p) {
+    return p.id == req.params.id;
+  });
+  res.render("posts", { post });
+});
+
+app.post("/posts", (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  let newPost = { id: posts.length + 1, title: title, content: content };
+  posts.push(newPost);
+  res.redirect("/");
 });
