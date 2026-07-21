f358ecbfd031a802afa5fd95ed3e623f6bb1a504
S. SAHIL <sahilshaik2849@gmail.com>
2026-07-21 12:35:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,93 +1,38 @@
+// HINTS:
+// 1. Import express and axios
+
+// 2. Create an express app and set the port number.
+
+// 3. Use the public folder for static files.
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+// 6. Listen on your predefined port and start the server
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://secrets-api.appbrewery.com";
-
-// HINTs: Use the axios documentation as well as the video lesson to help you.
-// https://axios-http.com/docs/post_example
-// Use the Secrets API documentation to figure out what each route expects and how to work with it.
-// https://secrets-api.appbrewery.com/
-
-//TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "8d5182b1-cc39-4396-94ee-f776c03825fa";
-const config = {
-  headers: { Authorization: `Bearer ${yourBearerToken}` },
-};
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.render("index.ejs", { content: "Waiting for data..." });
-});
-
-app.post("/get-secret", async (req, res) => {
-  const searchId = req.body.id;
-  try {
-    const result = await axios.get(API_URL + "/secrets/" + searchId, config);
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-  }
-});
-
-app.post("/post-secret", async (req, res) => {
-  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
-  try {
-    const result = await axios.post(
-      API_URL + "/secrets",
-      { secret: req.body.secret, score: req.body.score },
-      config,
-    );
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-  }
-});
-
-app.post("/put-secret", async (req, res) => {
-  const searchId = req.body.id;
-  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
-  try {
-    const result = await axios.put(
-      API_URL + "/secrets/" + searchId,
-      { secret: req.body.secret, score: req.body.score },
-      config,
-    );
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-  }
-});
-
-app.post("/patch-secret", async (req, res) => {
-  const searchId = req.body.id;
-  // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
-  try {
-    const result = await axios.patch(
-      API_URL + "/secrets/" + searchId,
-      { secret: req.body.secret, score: req.body.score },
-      config,
-    );
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-  }
-});
-
-app.post("/delete-secret", async (req, res) => {
-  const searchId = req.body.id;
-  // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
-  try {
-    const result = await axios.delete(API_URL + "/secrets/" + searchId, config);
-    res.render("index.ejs", { content: JSON.stringify(result.data) });
-  } catch (error) {
-    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
-  }
-});
+const API_URL = "https://secrets-api.appbrewery.com/random";
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log("server is running on port:", port);
+});
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL);
+    res.render("index.ejs", {
+      user: result.data.username,
+      secret: result.data.secret,
+    });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
