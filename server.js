0beb73d6febae1014eb4d3883b92bb700cf8d903
S. SAHIL <sahilshaik2849@gmail.com>
2026-07-28 12:19:55 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import axios from "axios";
 const app = express();
 const PORT = 8080;
 
@@ -7,6 +8,21 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.get("/explore", async(req,res)=>{
+  try {
+    const query = req.query.q
+    console.log("Search query:", query);
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/name/${query}`,
+    );
+    console.log(response.data);
+    res.render("explore.ejs",{countries:response.data});
+  }catch (error){
+    console.log(error.message);
+    res.status(404).send("country not Found");
+  }
 });
 
 app.listen(PORT, () => {
